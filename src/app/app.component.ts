cbd1c2e5c3f2be3054ef041c83a35329d746fd49
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2022-06-06 20:05:19 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Problema2';
+  title = 'Problema 2';
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    mensaje: new FormControl('', Validators.required),
+    respuesta: new FormControl('', Validators.required),
+  })
+
+  randomNum() {
+    let numero = Math.floor(Math.random() * 6);
+    return numero;
+  }
+
+  valorA = this.randomNum();
+  valorB = this.randomNum();
+
+  get nombre(){return this.formularioContacto.get('nombre')}
+  get email(){return this.formularioContacto.get('email')}
+  get mensaje(){return this.formularioContacto.get('mensaje')}
+  get respuesta(){return this.formularioContacto.get('mensaje')}
+
 }
